6394ccb1c72c2d8dfeb3070a4e32506fd08c9982
Samuel Akopyan <leumas.a@gmail.com>
2023-03-10 23:14:21 +0200

    Fix for PHP 7.0.3 in plural() function

--- framework/helpers/CString.php
+++ framework/helpers/CString.php
@@ -155,46 +155,48 @@ class CString
 	 * @param string $string
 	 * @return string
 	 */
-	function plural($string)
-	{
-		$result = strval($string);
+    function plural($string)
+    {
+        $result = (string)$string;
 
-		if(!is_countable($result)){
-			return $result;
-		}
+        if (PHP_VERSION_ID > 70300) {
+            if (!is_countable($result)) {
+                return $result;
+            }
+        }
 
-		$pluralRules = array(
-			'/(quiz)$/'                => '\1zes',      // quizzes
-			'/^(ox)$/'                 => '\1\2en',     // ox
-			'/([m|l])ouse$/'           => '\1ice',      // mouse, louse
-			'/(matr|vert|ind)ix|ex$/'  => '\1ices',     // matrix, vertex, index
-			'/(x|ch|ss|sh)$/'          => '\1es',       // search, switch, fix, box, process, address
-			'/([^aeiouy]|qu)y$/'       => '\1ies',      // query, ability, agency, jewelry
-			'/(hive)$/'                => '\1s',        // archive, hive
-			'/(?:([^f])fe|([lr])f)$/'  => '\1\2ves',    // half, safe, wife
-			'/sis$/'                   => 'ses',        // basis, diagnosis
-			'/([ti])um$/'              => '\1a',        // datum, medium
-			'/(p)erson$/'              => '\1eople',    // person, salesperson
-			'/(m)an$/'                 => '\1en',       // man, woman, spokesman
-			'/(c)hild$/'               => '\1hildren',  // child
-			'/(buffal|tomat)o$/'       => '\1\2oes',    // buffalo, tomato
-			'/(bu|campu)s$/'           => '\1\2ses',    // bus, campus
-			'/(alias|status|virus)$/'  => '\1es',       // alias
-			'/(octop)us$/'             => '\1i',        // octopus
-			'/(ax|cris|test)is$/'      => '\1es',       // axis, crisis
-			'/s$/'                     => 's',          // no change (compatibility)
-			'/$/'                      => 's',
-		);
+        $pluralRules = array(
+            '/(quiz)$/' => '\1zes',      // quizzes
+            '/^(ox)$/' => '\1\2en',     // ox
+            '/([m|l])ouse$/' => '\1ice',      // mouse, louse
+            '/(matr|vert|ind)ix|ex$/' => '\1ices',     // matrix, vertex, index
+            '/(x|ch|ss|sh)$/' => '\1es',       // search, switch, fix, box, process, address
+            '/([^aeiouy]|qu)y$/' => '\1ies',      // query, ability, agency, jewelry
+            '/(hive)$/' => '\1s',        // archive, hive
+            '/(?:([^f])fe|([lr])f)$/' => '\1\2ves',    // half, safe, wife
+            '/sis$/' => 'ses',        // basis, diagnosis
+            '/([ti])um$/' => '\1a',        // datum, medium
+            '/(p)erson$/' => '\1eople',    // person, salesperson
+            '/(m)an$/' => '\1en',       // man, woman, spokesman
+            '/(c)hild$/' => '\1hildren',  // child
+            '/(buffal|tomat)o$/' => '\1\2oes',    // buffalo, tomato
+            '/(bu|campu)s$/' => '\1\2ses',    // bus, campus
+            '/(alias|status|virus)$/' => '\1es',       // alias
+            '/(octop)us$/' => '\1i',        // octopus
+            '/(ax|cris|test)is$/' => '\1es',       // axis, crisis
+            '/s$/' => 's',          // no change (compatibility)
+            '/$/' => 's',
+        );
 
-		foreach ($pluralRules as $rule => $replacement){
-			if(preg_match($rule, $result)){
-				$result = preg_replace($rule, $replacement, $result);
-				break;
-			}
-		}
-		
-		return $result;
-	}
+        foreach ($pluralRules as $rule => $replacement) {
+            if (preg_match($rule, $result)) {
+                $result = preg_replace($rule, $replacement, $result);
+                break;
+            }
+        }
+
+        return $result;
+    }
 
     /**
      * Checks is a given string is serialized 
